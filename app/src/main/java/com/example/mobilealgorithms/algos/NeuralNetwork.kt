596c63987f2b63ddaf65a7b7a490377fde3422da
Artem Manukhin <Artem.manukhin@mail.ru>
2026-03-28 14:48:19 +0700

    Linear layer implemented (not tested)

--- app/src/main/java/com/example/mobilealgorithms/algos/NeuralNetwork.kt
+++ app/src/main/java/com/example/mobilealgorithms/algos/NeuralNetwork.kt
@@ -12,52 +12,52 @@ private class Matrix {
         return Pair(size.first, size.second)
     }
     constructor(size: Pair<Int, Int>, fillRandom: Boolean = false) {
-        this.size = size.copy();
+        this.size = size.copy()
         matrix = Array(size.first) { DoubleArray(size.second) {
-            if (fillRandom) Random.nextDouble(0.0, 1.0) else 0.0
+            if (fillRandom) Random.nextDouble(-0.5, 0.5) else 0.0
         } }
     }
     companion object {
-        fun copy(matrix: Matrix) : Matrix {
-            val result = Matrix(matrix.size)
-            for (i in 0 until matrix.size.first) {
-                for (j in 0 until matrix.size.second) {
-                    result.matrix[i][j] = matrix.matrix[i][j]
+        fun copy(m: Matrix) : Matrix {
+            val result = Matrix(m.size)
+            for (i in 0 until m.size.first) {
+                for (j in 0 until m.size.second) {
+                    result.matrix[i][j] = m.matrix[i][j]
                 }
             }
             return result
         }
-        fun transpose(matrix: Matrix): Matrix {
-            val result = Matrix(Pair(matrix.size.second, matrix.size.first))
-            for (i in 0 until matrix.size.first) {
-                for (j in 0 until matrix.size.second) {
-                    result.matrix[j][i] = matrix.matrix[i][j]
+        fun transpose(m: Matrix): Matrix {
+            val result = Matrix(Pair(m.size.second, m.size.first))
+            for (i in 0 until m.size.first) {
+                for (j in 0 until m.size.second) {
+                    result.matrix[j][i] = m.matrix[i][j]
                 }
             }
             return result
         }
-        fun add(matrix1: Matrix, matrix2: Matrix) : Matrix {
-            if (matrix2.size.first != matrix1.size.first || matrix2.size.second != matrix1.size.second) {
+        fun add(m1: Matrix, m2: Matrix) : Matrix {
+            if (m2.size.first != m1.size.first || m2.size.second != m1.size.second) {
                 throw Exception("Incorrect matrix size")
             }
-            val result = Matrix(matrix1.size)
-            for (i in 0 until matrix1.size.first) {
-                for (j in 0 until matrix1.size.second) {
-                    result.matrix[i][j] = matrix1.matrix[i][j] + matrix2.matrix[i][j]
+            val result = Matrix(m1.size)
+            for (i in 0 until m1.size.first) {
+                for (j in 0 until m1.size.second) {
+                    result.matrix[i][j] = m1.matrix[i][j] + m2.matrix[i][j]
                 }
             }
             return result
         }
-        fun mul(matrix1: Matrix, matrix2: Matrix) : Matrix {
+        fun mul(m1: Matrix, m2: Matrix) : Matrix {
             /* matrix1 * matrix2 */
-            if (matrix1.size.second != matrix2.size.first) {
+            if (m1.size.second != m2.size.first) {
                 throw Exception("Incorrect matrix size")
             }
-            val result = Matrix(Pair(matrix1.size.first, matrix2.size.second))
-            for (i in 0 until matrix1.size.first) {
-                for (j in 0 until matrix2.size.second) {
-                    for (k in 0 until matrix1.size.second) {
-                        result.matrix[i][j] += matrix1.matrix[i][k] * matrix2.matrix[k][j]
+            val result = Matrix(Pair(m1.size.first, m2.size.second))
+            for (i in 0 until m1.size.first) {
+                for (j in 0 until m2.size.second) {
+                    for (k in 0 until m1.size.second) {
+                        result.matrix[i][j] += m1.matrix[i][k] * m2.matrix[k][j]
                     }
                 }
             }
@@ -72,5 +72,53 @@ private class Matrix {
             }
             return result
         }
+    }
+}
+
+private data class NNParameters(val values: Matrix, val gradient: Matrix) {}
+private abstract class NNModule {
+    abstract fun forward(inputData: Matrix) : Matrix
+    abstract fun backward(outputGrad: Matrix) : Matrix
+    abstract fun zeroGrad()
+    abstract fun parameters() : List<NNParameters>
+}
+
+private class Linear (height: Int, width: Int) : NNModule() {
+    private val weights = Matrix(Pair(height, width), true)
+    private var weightsGrad = Matrix(weights.getSize(), false)
+    private val bias = Matrix(Pair(1, width), true)
+    private var biasGrad = Matrix(bias.getSize(), false)
+    private var inputData: Matrix? = null
+    override fun forward(inputData: Matrix) : Matrix {
+        this.inputData = inputData
+        return Matrix.add(Matrix.mul(inputData, Matrix.transpose(weights)),
+                          bias)
+        /* y = x * W^T + b */
+    }
+    override fun parameters(): List<NNParameters> {
+        return listOf(NNParameters(weights, weightsGrad),
+                      NNParameters(bias, biasGrad))
+    }
+    override fun zeroGrad() {
+        for (line in weightsGrad.matrix) {
+            line.fill(0.0)
+        }
+        biasGrad.matrix[0].fill(0.0)
+    }
+    override fun backward(outputGrad: Matrix): Matrix {
+        val inputGrad = Matrix.mul(outputGrad, weights)
+        /* dL/dx = dL/dy * dy/dx, dy/dx = W */
+        val weightsGrad = Matrix.mul(Matrix.transpose(outputGrad), inputData!!)
+        /* dL/dW = dL/dy * dy/dW, dy/dW = x */
+        val biasGrad = Matrix(bias.getSize(), false)
+        /* dL/db = dL/dy * dy/db, dy/db = 1 */
+        for (i in 0 until outputGrad.getSize().first) {
+            for (j in 0 until outputGrad.getSize().second) {
+                biasGrad.matrix[0][j] += outputGrad.matrix[i][j]
+            }
+        }
+        this.weightsGrad = Matrix.add(this.weightsGrad, weightsGrad)
+        this.biasGrad = Matrix.add(this.biasGrad, biasGrad)
+        return inputGrad
     }
 }
